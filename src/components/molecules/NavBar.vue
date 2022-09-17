c15764318cd6e56c965ec1e62fa37cb8e6e998ed
Kevin Almeida <janioklalmeida05@gmail.com>
2022-09-17 13:17:46 -0300

    🎉 inital commit

---
+++ src/components/molecules/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="nav-bar">
+    <NavItem text="Serviços" to="/services" />
+    <NavItem text="Sobre mim" to="/about" />
+    <NavItem text="Cases" to="/cases" />
+    <NavItem text="Contato" to="/contact" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { NavItem } from '@/components/atoms'
+
+export default defineComponent({
+  components: { NavItem }
+})
+</script>
+
+<style lang="scss" scoped>
+.nav-bar {
+  width: max-content;
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: 2rem;
+}
+</style>
---
+++ src/components/organisms/Services.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="services">
+    <Service
+      title="Front End"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eum explicabo quaerat fuga reiciendis officia pariatur dolores, libero quam numquam odit. Reprehenderit in voluptate excepturi, tempora officia beatae quae veritatis!"
+    />
+    <Service
+      title="Back End"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eum explicabo quaerat fuga reiciendis officia pariatur dolores, libero quam numquam odit. Reprehenderit in voluptate excepturi, tempora officia beatae quae veritatis!"
+    />
+    <Service
+      title="Manutenção"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eum explicabo quaerat fuga reiciendis officia pariatur dolores, libero quam numquam odit. Reprehenderit in voluptate excepturi, tempora officia beatae quae veritatis!"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Service } from '@/components/molecules'
+
+export default defineComponent({ components: { Service } })
+</script>
+
+<style lang="scss" scoped>
+.services {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 5rem;
+}
+</style>
